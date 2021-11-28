088dd4c42a47aa427d0e7dc9ed6e8325b44b22e3
essenija1 <80848058+essenija1@users.noreply.github.com>
2021-11-28 12:10:43 +0300

    Create DifferenceTime.js

---
+++ DifferenceTime.js
@@ -0,0 +1,9 @@
+// Для отображения финального номера дней (результат)
+
+document.write("Total number of days between dates  <br>"
+
+               + date1 + "<br> and <br>" 
+
+               + date2 + " is: <br> " 
+
+               + Difference_In_Days);
